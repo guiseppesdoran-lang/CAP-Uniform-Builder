5ff916b566649738043907e8b828c4a98c0c4b63
guiseppesdoran-lang <guiseppe.s.doran@gmail.com>
2026-08-05 05:03:32 -0400

    Populate Android app: app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.capuniformbuilder.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.capuniformbuilder.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
